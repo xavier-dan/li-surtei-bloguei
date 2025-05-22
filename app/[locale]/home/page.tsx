8c53bdbab6563153fc4934b33b28d12a4b635621
Daniel Lacerda Xavier <dani.lacerda06@hotmail.com>
2025-05-22 13:55:54 -0300

    delete page.tsx

--- app/[locale]/home/page.tsx
+++ app/[locale]/home/page.tsx
@@ -3,7 +3,6 @@ import Hero from '../../components/organisms/hero/Hero';
 import LatestReviewSect from '../../components/organisms/latestReviewSect/LatestReviewSect';
 import Footer from "../../components/organisms/footer/Footer";
 
-
 export default function Start() {
     return (
         <>
